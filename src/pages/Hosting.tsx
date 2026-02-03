1d6e9508183d2ac92ae340ef6cb7cbb684c59447
TetianaKhomiak <tkhomiak8889@gmail.com>
2026-02-03 22:29:10 +0100

    Added paragraph to Pages with no main section

--- src/pages/Hosting.tsx
+++ src/pages/Hosting.tsx
@@ -9,6 +9,12 @@ const Hosting = () => {
           Gostovanje
         </h1>
       </div>
+      <div
+        className="bg-white md:bg-white dark:bg-gray-900
+        md:bg-[radial-gradient(#d1d5db_1px,transparent_1px)] md:dark:bg-[radial-gradient(#4b5563_1px,transparent_1px)]
+        md:bg-[length:16px_16px] h-[120px] font-(family-name:--font-anonymous) font-normal text-[16px] dark:text-gray-400">
+        <p className="p-10">Novice bodo objavljene kmalu...</p>
+      </div>
     </div>
   );
 };
